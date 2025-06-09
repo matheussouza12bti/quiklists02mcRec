3f9c41630128bc0bdb17a4d6c74ccd59f5d5226a
matheus souza <math.souzatrabalhos@gmail.com>
2025-06-09 00:18:17 -0300

    criação da função showItensList para renderizar os intes do array na tela

--- main.js
+++ main.js
@@ -7,12 +7,36 @@ function addItem() {
     const item = {
         name: itemName,
         checkd: false
-    }
-}
+        }
 
-
-    items.push(item) {
+        items.push(item) 
+        
         document.querySelector("#item").value = ""
 
-        console.log(items)
+        showItensList()
     }
+
+    function showItemList() { 
+        const sectionList = document.querySelector(".list")
+        sectionList.textContent = ""
+
+
+            items.map((item, index) => {
+                sectionList.innerHTML += `
+                <div class="item">
+                    <div>
+                        <input type="checkbox" name="list" id="iten-${index}">
+                        <div class="custom-checkbox">
+                            <img src="./assets/assets/checked.svg" alt="checkd">
+                        </div>
+                        <label for="iten-${index}">Maçã</label>
+                    </div>
+                    <button><img src="./assets/assets/trash-icon.svg" alt="trach icon"></button>
+                </div>                
+                `
+            }) 
+        }
+    
+
+    
+
