d445e3f1dc0463a5b6293d8a81eed824e77c77b6
matheus souza <math.souzatrabalhos@gmail.com>
2025-06-08 23:23:42 -0300

    criação do arquivo main.js, criação do array do items e criação da função para adicionar novos itens no array

---
+++ main.js
@@ -0,0 +1,18 @@
+const items = []
+
+
+function addItem() {
+    const itemName = document.querySelector ("#item").value
+
+    const item = {
+        name: itemName,
+        checkd: false
+    }
+}
+
+
+    items.push(item) {
+        document.querySelector("#item").value = ""
+
+        console.log(items)
+    }
